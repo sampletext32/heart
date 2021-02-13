4d2ee55e0ebe5f65eadbd1994f5d68be57d7a1da
Bird Egop <sampletext32@bk.ru>
2021-02-14 01:30:10 +0300

    Add project

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const heartuptimer = setInterval(heartup, 200);
+
+function heartup() {
+    $(".heart").css("font-weight", "900");
+}
+
+const heartdowntimer = setInterval(heartdown, 500);
+
+function heartdown() {
+    $(".heart").css("font-weight", "100");
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    let filePath = '.' + request.url.split("?")[0];
+    if (filePath === './')
+        filePath = './index.html';
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
